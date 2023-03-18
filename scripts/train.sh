604e61f7a78d5562fff2ed3f806431e53d71e06f
nthphucai <nthphucai@gmail.com>
2023-03-18 11:35:10 +0000

    chores: update script and bash files

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,10 +3,11 @@ set -e
 
 python segment/training/train.py \
     --model_name_or_path UnetRes_v2 \
-    --output_dir output/models \
+    --output_dir output/models/best_model_1803.pt \
     --config_dir configs/segment_pipeline.yaml \
     --num_classes 2 \
     --act_func sigmoid \
     --data_path output/data/train_dataset.pt \
     --class_weight_path output/class_weight.npy \
-    
+    --num_train_epochs 3 \
+    --log_dir output/logs_1803.csv \
