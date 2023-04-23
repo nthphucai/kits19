e33f655fa81ee10ca183bace78eaa3473b3cfc3b
nthphucai <nthphucai@gmail.com>
2023-04-23 09:10:33 +0000

    training(optimizer): add zero_grad to lookahead

--- segment/training/optimizers/lookahead.py
+++ segment/training/optimizers/lookahead.py
@@ -1,12 +1,9 @@
-import warnings
-from collections import defaultdict
 from itertools import chain
+from collections import defaultdict
 
 import torch
 from torch.optim import Optimizer
 
-
-# Look ahead optimizer
 class Lookahead(Optimizer):
     def __init__(self, optimizer, k=5, alpha=0.5):
         self.optimizer = optimizer
@@ -17,7 +14,7 @@ class Lookahead(Optimizer):
         self.fast_state = self.optimizer.state
         for group in self.param_groups:
             group["counter"] = 0
-
+    
     def update(self, group):
         for fast in group["params"]:
             param_state = self.state[fast]
@@ -27,10 +24,13 @@ class Lookahead(Optimizer):
             slow = param_state["slow_param"]
             slow += (fast.data - slow) * self.alpha
             fast.data.copy_(slow)
-
+    
     def update_lookahead(self):
         for group in self.param_groups:
             self.update(group)
+
+    def zero_grad(self, set_to_none: bool = False):
+        self.optimizer.zero_grad()
 
     def step(self, closure=None):
         loss = self.optimizer.step(closure)
@@ -72,3 +72,4 @@ class Lookahead(Optimizer):
     def add_param_group(self, param_group):
         param_group["counter"] = 0
         self.optimizer.add_param_group(param_group)
+        
