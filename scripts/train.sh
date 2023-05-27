e2fd8572d73221f6882c84130f5a13be67bc3103
nthphucai <nthphucai@gmail.com>
2023-05-27 19:57:09 +0700

    fix(scripts/train): fix cache_dir

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,14 +3,16 @@ set -e
 
 python segment/training/train.py \
     --model_name_or_path UnetRes_v2 \
-    --output_dir output/models/best_model_2403_softmax.pt \
-    --cache_dir="output/models/best_model_1903_softmax.pt" \
-    --train_dataset_path output/data/train_dataset.pt \
+    --output_dir output/models/fold_1/best_model_1405_softmax.pt \
+    --train_dataset_path output/data/valid_dataset.pt \
     --valid_dataset_path output/data/valid_dataset.pt \
     --config_dir configs/segment_pipeline.yaml \
     --class_weight_path output/class_weight.npy \
-    --num_train_epochs 5 \
-    --log_dir output/logs_2403.csv \
+    --num_train_epochs 3 \
+    --log_dir output/logs/fold_1/logs_1405_softmax.csv \
     --freeze_feature False \
     --do_train True \
-    --do_eval True 
+    --do_eval False \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --report_to "wandb"
